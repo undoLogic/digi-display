509a18a6e3c6a9082219ef8926c2f725fdf2ebcf
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-09-30 10:13:45 -0400

    preparing concept system in phases

--- 1-download.sh
+++ 1-download.sh
@@ -6,7 +6,7 @@ mkdir offlineBox
 cd offlineBox
 git clone https://github.com/undoLogic/offlinebox.git --branch main --single-branch .
 
-sudo chmod +x -R offlineBox
+sudo chmod +x -R *
 
 #wget https://raw.githubusercontent.com/username/repository/branch/path/filename.md
 #wget https://raw.githubusercontent.com/undoLogic/offlineBox/main/download.sh
