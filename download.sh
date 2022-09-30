40013fd4909e55126959853fc3dda8a26d3492c1
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-09-30 06:53:00 -0400

    setting up get

--- download.sh
+++ download.sh
@@ -2,4 +2,10 @@
 
 # get this file to the new computer
 mkdir offlineBox
+cd offlineBox
 git clone https://github.com/undoLogic/offlinebox.git --branch main --single-branch .
+
+
+
+#wget https://raw.githubusercontent.com/username/repository/branch/path/filename.md
+#wget https://raw.githubusercontent.com/undoLogic/offlineBox/main/download.sh
