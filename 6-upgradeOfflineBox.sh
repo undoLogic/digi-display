95124620c9ec2a7c50fee68d9be528ed2d2350d6
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-10-01 12:09:02 -0400

    testing

--- 6-upgradeOfflineBox.sh
+++ 6-upgradeOfflineBox.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
-# maintenance mode
-# ssh tunnel into a random server created on the fly (for security)
-# allowing staff to login to this machine and perform maintenance
+# force upgrade all files of the offlineBox
 
-MAINTENANCE_SERVER="root@192.46.222.20"
-PORT=43022
-
-# setup tunnel REMOTE -> VPS
-#ssh -R 43022:localhost:22 -i id_rsa root@147.182.145.17
-
-ssh -R $PORT:localhost:22 -i id_rsa $MAINTENANCE_SERVER
-
-#then login to VPS and establish tunnel back to REMOTE
-#ssh localhost -p 43022
+cd ~/offlineBox || exit
+git reset --hard HEAD
+git pull
+chmod +x *
---
+++ 7-offlineBoxMaintenance.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# maintenance mode
+# ssh tunnel into a random server created on the fly (for security)
+# allowing staff to login to this machine and perform maintenance
+
+MAINTENANCE_SERVER="root@192.46.222.20"
+PORT=43022
+
+# setup tunnel REMOTE -> VPS
+#ssh -R 43022:localhost:22 -i id_rsa root@147.182.145.17
+
+ssh -R $PORT:localhost:22 -i id_rsa $MAINTENANCE_SERVER
+
+#then login to VPS and establish tunnel back to REMOTE
+#ssh localhost -p 43022
