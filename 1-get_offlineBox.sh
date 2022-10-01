20a73ef7e5620165ae8108945568fa8cdb1c9202
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-10-01 11:58:08 -0400

    setting up the ssh tunnel

--- 1-get_offlineBox.sh
+++ 1-get_offlineBox.sh
@@ -6,9 +6,8 @@ sudo apt update
 sudo apt install git
 
 # get this file to the new computer
-cd ~/ || exit
-mkdir offlineBox || exit
-cd offlineBox || exit
+mkdir ~/offlineBox || return
+cd ~/offlineBox || exit
 git clone https://github.com/undoLogic/offlinebox.git --branch main --single-branch .
 
 # fix permissions @todo find better permissions
