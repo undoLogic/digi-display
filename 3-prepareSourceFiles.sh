6a5f5dd65c248e0ef97c5650398ee12013231ca6
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-10-01 12:05:42 -0400

    testing

--- 3-prepareSourceFiles.sh
+++ 3-prepareSourceFiles.sh
@@ -2,10 +2,9 @@
 
 # checkout files from github
 cd ~/offlineBox || exit
-cd sites/domain.com || exit
+cd sites/primary || exit
 
 #cd /home/liquidle/www/staging && rm -rf master && git clone git@github.com:undologic/updateCase-undologic.git --branch master --single-branch /home/liquidle/www/staging/master
 git clone git@github.com:undologic/updateCase-undologic.git --branch master --single-branch .
-
 
 chmod 777 -R *
