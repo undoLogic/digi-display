aeb7b2e0298ff186ef998889a462cd1ee70e3bf8
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-10-01 22:09:36 -0400

    starting cron - autossh..

--- 7-offlineBoxMaintenance.sh
+++ 7-offlineBoxMaintenance.sh
@@ -9,9 +9,31 @@ PORT=$(jq -r '.maintenance_server_tunnel_port' config.json)
 
 ########################################################################################################################
 # setup tunnel REMOTE -> VPS
-#@todo create a non root user for higher security access
-ssh -R "$PORT":root@localhost:22 "$MAINTENANCE_SERVER"
-#eg ssh -R 43022:root@localhost:22 root@maintenance.offlinebox.com
+
+SERVICE="autossh"
+if pgrep -x "$SERVICE" >/dev/null
+then
+    echo "$SERVICE is already running"
+else
+    echo "$SERVICE stopped"
+
+    #@todo create a non root user for higher security access
+    logger "Setting up reverse SSH tunnel: ssh -R $PORT:root@localhost:22 $MAINTENANCE_SERVER"
+    #ssh -R "$PORT":root@localhost:22 "$MAINTENANCE_SERVER"
+    autossh -f -R "$PORT":root@localhost:22 "$MAINTENANCE_SERVER" &
+    #eg ssh -R 43022:root@localhost:22 root@maintenance.offlinebox.com
+    #autossh -R 43022:localhost:22 root@maintenance.offlinebox.com
+
+    # uncomment to start nginx if stopped
+    # systemctl start nginx
+    # mail
+fi
+
+
+
+
+
+
 
 ########## then login to VPS and establish tunnel back to REMOTE
 # ssh offlinebox@localhost -p 43022
--- 9-cron.sh
+++ 9-cron.sh
@@ -2,22 +2,29 @@
 
 # this will be run as a crontab
 
+# see if a new config exists for this box ?
+
 # read from our config
 MAINTENANCE_ACTIVE=$(jq -r '.maintenance_active' config.json)
 echo "maintenance_active is $MAINTENANCE_ACTIVE"
 if [ "$MAINTENANCE_ACTIVE" == "true" ]; then
   logger "OfflineBox-CRON: ensure maintenance mode is active"
-  #./7-offlinBoxMaintenance.sh
+  ./7-offlinBoxMaintenance.sh
 else
-    logger "OfflineBox-CRON: IGNORE maintenance mode"
+  logger "OfflineBox-CRON: IGNORE maintenance mode - ensure autossh is not running"
+  pgrep -x autossh | xargs kill -9
 fi
+
+
+
+
 
 RUN=$(jq -r '.run' config.json)
 echo "run is $RUN"
 
 if [ "$RUN" == "true" ]; then
   logger "OfflineBox-CRON: ensure run !"
-  #./7-offlinBoxMaintenance.sh
+  ./4-run.sh
 else
-    logger "OfflineBox-CRON: IGNORE run / disabled"
+    logger "OfflineBox-CRON: IGNORE run / NOT RUNNING"
 fi
