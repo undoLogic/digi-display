79672c926b0409bd9805c51115bdff61d8cdf6ff
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-10-01 22:17:13 -0400

    starting cron - autossh..

--- 4-run.sh
+++ 4-run.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
 
-# general tasks on run
-# verify ssh tunnel is active and working
+# @todo check if docker is already running
 
-# check our state
-STATE=$(jq -r '.state' config.json)
+# start / verify docker is running
+cd ~/Desktop/offlineBox/sites/primary/docker/ || exit
+# start docker
+./1reStartDocker.sh
+# this should be moved into a config - what the starting page is
 
-if [ "$STATE" == "RUN" ]; then
-  # run
-  echo "STATE is set to RUN"
-  # start / verify docker is running
-  cd ~/Desktop/offlineBox/sites/primary/docker/ || exit
-  # start docker
-  ./1reStartDocker.sh
-  # this should be moved into a config - what the starting page is
+#make sure it is closed as if it is open only a new tab will open
+# shellcheck disable=SC2046
+kill -9 $(ps -x | grep firefox)
 
-  #make sure it is closed as if it is open only a new tab will open
-  kill -9 $(ps -x | grep firefox)
-
-  firefox -kiosk http://localhost/sourceFiles/pages/signage
-else
-  # do not run
-  echo "cannot run as state is $STATE"
-fi
+firefox -kiosk http://localhost/sourceFiles/pages/signage
 
 
 
