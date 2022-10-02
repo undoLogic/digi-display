839b00c4ef1fbfa162cad6737d8e0cf05e676e10
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-10-01 22:20:01 -0400

    starting cron - autossh..

--- 7-offlineBoxMaintenance.sh
+++ 7-offlineBoxMaintenance.sh
@@ -20,7 +20,7 @@ else
     #@todo create a non root user for higher security access
     logger "Setting up reverse SSH tunnel: ssh -R $PORT:root@localhost:22 $MAINTENANCE_SERVER"
     #ssh -R "$PORT":root@localhost:22 "$MAINTENANCE_SERVER"
-    autossh -f -R "$PORT":root@localhost:22 "$MAINTENANCE_SERVER" &
+    autossh -f -R "$PORT":root@localhost:22 "$MAINTENANCE_SERVER"
     #eg ssh -R 43022:root@localhost:22 root@maintenance.offlinebox.com
     #autossh -R 43022:localhost:22 root@maintenance.offlinebox.com
 
