96fbe92173592d22fd1e1b871b1331a6ae10d1b1
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-10-01 21:11:20 -0400

    starting cron

--- 9-cron.sh
+++ 9-cron.sh
@@ -5,9 +5,9 @@
 # read from our config
 MAINTENANCE_ACTIVE=$(jq -r '.maintenance_active' config.json)
 echo "maintenance_active is $MAINTENANCE_ACITVE"
-if [ "$MAINTENANCE_ACITVE" == true ]; then
+if [ "$MAINTENANCE_ACITVE" == "true" ]; then
   logger "OfflineBox-CRON: ensure maintenance mode is active"
-  ./7-offlinBoxMaintenance.sh
+  #./7-offlinBoxMaintenance.sh
 else
     logger "OfflineBox-CRON: IGNORE maintenance mode"
 fi
@@ -15,9 +15,9 @@ fi
 RUN=$(jq -r '.run' config.json)
 echo "run is $RUN"
 
-if [ "$MAINTENANCE_ACITVE" == true ]; then
+if [ "$MAINTENANCE_ACITVE" == "true" ]; then
   logger "OfflineBox-CRON: ensure run !"
-  ./7-offlinBoxMaintenance.sh
+  #./7-offlinBoxMaintenance.sh
 else
     logger "OfflineBox-CRON: IGNORE run / disabled"
 fi
