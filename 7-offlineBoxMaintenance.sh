1879159cd6e6074763367e8900279872012c33f7
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-10-07 18:44:54 -0400

    cleaning up

--- 7-offlineBoxMaintenance.sh
+++ 7-offlineBoxMaintenance.sh
@@ -14,15 +14,17 @@ then
       logger -s "OfflineBox: Maintenance server already running (use first arg 'stop' if you want to stop)"
     fi
 else
-    logger "OfflineBox: Starting MAINTENANCE_SERVER - Setting up reverse SSH tunnel: ssh -R $PORT:root@localhost:22 $MAINTENANCE_SERVER"
-
-    #@todo create a non root user for higher security access
-    #autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -fN -T -R 10000:localhost:22 console@<myvpnip>
-    autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -fN -T -R "$PORT":localhost:22 "$MAINTENANCE_SERVER"
-
-    ########## then login to VPS and establish tunnel back to REMOTE
-    # ssh offlinebox@localhost -p 43022
-
-    # Manual reverse tunnel
-    #ssh -R "$PORT":localhost:22 "$MAINTENANCE_SERVER"
+  if [ "$1" == 'stop' ]
+    then
+      logger -s "OfflineBox: Already shutdown"
+    else
+      logger -s "OfflineBox: Starting MAINTENANCE_SERVER - Setting up reverse SSH tunnel: ssh -R $PORT:root@localhost:22 $MAINTENANCE_SERVER"
+      #@todo create a non root user for higher security access
+      #autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -fN -T -R 10000:localhost:22 console@<myvpnip>
+      autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -fN -T -R "$PORT":localhost:22 "$MAINTENANCE_SERVER"
+      ########## then login to VPS and establish tunnel back to REMOTE
+      # ssh offlinebox@localhost -p 43022
+      # Manual reverse tunnel
+      #ssh -R "$PORT":localhost:22 "$MAINTENANCE_SERVER"
+    fi
 fi
