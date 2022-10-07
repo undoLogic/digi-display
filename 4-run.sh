b2172b71ff30db43b3ea2f55d5fcd65b57f5b126
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-10-07 06:53:47 -0400

    starting checks if firefox is running

--- 4-run.sh
+++ 4-run.sh
@@ -5,17 +5,18 @@ if pgrep -x "firefox" > /dev/null
 then
 
       logger "offlineBox: Starting firefox"
-      # start / verify docker is running
-      cd ~/Desktop/offlineBox/sites/primary/docker/ || exit
-      # start docker
-      ./1reStartDocker.sh
-      # this should be moved into a config - what the starting page is
 
-      #make sure it is closed as if it is open only a new tab will open
-      # shellcheck disable=SC2046
-      kill -9 $(ps -x | grep firefox)
-
-      firefox -kiosk http://localhost/sourceFiles/pages/signage
+#      # start / verify docker is running
+#      cd ~/Desktop/offlineBox/sites/primary/docker/ || exit
+#      # start docker
+#      ./1reStartDocker.sh
+#      # this should be moved into a config - what the starting page is
+#
+#      #make sure it is closed as if it is open only a new tab will open
+#      # shellcheck disable=SC2046
+#      kill -9 $(ps -x | grep firefox)
+#
+#      firefox -kiosk http://localhost/sourceFiles/pages/signage
 else
       echo "Firefox running"
       logger "offlineBox: Firefox already running"
