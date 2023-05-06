df4a07132a98027d3088ba97110330692e75796b
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2023-05-06 13:04:37 -0400

    init

--- init_digiDisplay.sh
+++ init_digiDisplay.sh
@@ -5,8 +5,9 @@ sudo apt-get update
 sudo apt-get install jq git -y
 
 cd ~/Desktop || exit # fail if it doens't exist something wrong
-mkdir ~/Desktop/digiDisplay || return # could already exist
-cd ~/Desktop/digiDisplay || exit # fail if it doens't exist something wrong
+rm -rf ~/Desktop/digiDisplay
+#mkdir ~/Desktop/digiDisplay || return # could already exist
+#cd ~/Desktop/digiDisplay || exit # fail if it doens't exist something wrong
 
 git clone https://github.com/undoLogic/digi-display.git --branch main --single-branch .
 
@@ -15,5 +16,5 @@ git clone https://github.com/undoLogic/digi-display.git --branch main --single-b
 # allow that the run kiosk can run as execute commands
 chmod +x runDigiDisplay.sh
 
-echo "NEXT: display power settings so the screen remains active -> settings > power > power mode PERFORMANCE > dim screen UNCHECK > screen blank OFF"
-echo "NEXT: add 'runDigiDisplay.sh' to startup applications"
+echo " =========== NEXT: display power settings so the screen remains active -> settings > power > power mode PERFORMANCE > dim screen UNCHECK > screen blank OFF"
+echo " =========== NEXT: add 'runDigiDisplay.sh' to startup applications"
