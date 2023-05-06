087ee4286b312aece6ae5e91c893a5e4fddbd4bb
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2023-05-06 11:54:26 -0400

    added remove

---
+++ remove.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd ~/Desktop || exit # fail if it doens't exist something wrong
+
+# remove all the files
+rm config.json
+rm .git
+rm .gitignore
+rm .idea
+rm index.php
+rm init_digiDisplay.sh
+rm LICENSE
+rm README.md
+rm runDigiDisplay.sh
