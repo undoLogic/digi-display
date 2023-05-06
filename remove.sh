e7e31250e2f77e759de377375846b1e42974f7f8
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2023-05-06 12:54:06 -0400

    init

--- remove.sh
+++ remove.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 cd ~/Desktop || exit # fail if it doens't exist something wrong
 
-# remove dirs
-rm -rf .git
-rm -rf .idea
+## remove dirs
+#rm -rf .git
+#rm -rf .idea
+#
+## remove all the files
+#rm config.json
+#rm .gitignore
+#rm index.php
+#rm init_digiDisplay.sh
+#rm LICENSE
+#rm README.md
+#rm runDigiDisplay.sh
+#rm remove.sh
 
-# remove all the files
-rm config.json
-rm .gitignore
-rm index.php
-rm init_digiDisplay.sh
-rm LICENSE
-rm README.md
-rm runDigiDisplay.sh
-rm remove.sh
+rm ~/Desktop/digiDisplay
