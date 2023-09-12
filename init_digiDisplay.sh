428b0f0c1ae32b923203508c698306345fed790a
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2023-09-12 12:30:22 -0400

    install script

--- init_digiDisplay.sh
+++ init_digiDisplay.sh
@@ -6,15 +6,14 @@ sudo apt-get install jq git -y
 
 cd ~/Desktop || exit # fail if it doens't exist something wrong
 rm -rf ~/Desktop/digiDisplay
-#mkdir ~/Desktop/digiDisplay || return # could already exist
-#cd ~/Desktop/digiDisplay || exit # fail if it doens't exist something wrong
+mkdir ~/Desktop/digiDisplay || return # could already exist
+cd ~/Desktop/digiDisplay || exit # fail if it doens't exist something wrong
 
 git clone https://github.com/undoLogic/digi-display.git --branch main --single-branch .
-
-# all the files are now in the desktop (i think it's better to have them out in the open for troubleshooting in the future)
 
 # allow that the run kiosk can run as execute commands
 chmod +x runDigiDisplay.sh
 
 echo " =========== NEXT: display power settings so the screen remains active -> settings > power > power mode PERFORMANCE > dim screen UNCHECK > screen blank OFF"
+echo " =========== NEXT: disable popups: sudo nano /etc/xdg/autostart/update-notifier.desktop => NoDisplay=true"
 echo " =========== NEXT: add 'runDigiDisplay.sh' to startup applications"
