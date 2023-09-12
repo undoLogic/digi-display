d7138c8530dc0ab08a40218ba753b38f4ed84705
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2023-09-12 13:09:43 -0400

    close firefox when starting

--- runDigiDisplay.sh
+++ runDigiDisplay.sh
@@ -6,6 +6,15 @@ while [ "$(hostname -I)" = "" ]; do
   sleep 2
 done
 
+
+# close firefox if it is running
+if pgrep firefox; then
+    # If Firefox is running, close it gracefully
+    pkill firefox
+    # Wait for a moment to allow Firefox to close
+    sleep 2
+fi
+
 url=$(jq -r '.url' ~/Desktop/digiDisplay/config.json)
 kiosk=$(jq -r '.kiosk' ~/Desktop/digiDisplay/config.json)
 echo "running url $url"
